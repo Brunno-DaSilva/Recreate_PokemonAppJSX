0c1d693178c72a89aebe0c0a82b048034e61f305
Bruno DaSilva <bananaatomica@Brunos-Air.attlocal.net>
2020-04-10 20:55:50 -0500

    I can see the pokemons

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,6 +1,7 @@
 const React = require("react");
 
 const headerContainer = {
+  fontFamily: "Arial",
   display: "flex",
   flexDirection: "column",
   justifyContent: "space-around",
@@ -8,11 +9,11 @@ const headerContainer = {
   width: "100vw",
   minHeight: "50vh",
   padding: "1rem",
+  margin: "-1rem",
   backgroundColor: "#01BAEF",
   fontSize: "3rem",
   color: "#0B4F6C",
   letterSpacing: "0.5rem",
-  fontFamily: "Arial",
 };
 
 const headerNavigation = {
@@ -32,6 +33,46 @@ const headerNavigationItems = {
 
 const headerNavigationItemsP = {
   fontSize: "2rem",
+  color: "#FBFBFF",
+};
+
+const bodyContainer = {
+  fontFamily: "Arial",
+  padding: "1.5rem",
+  width: "90vw",
+  height: "100vh",
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const bodyContainerCard = {
+  width: "26%",
+  maxHeight: "700px",
+  margin: "auto",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "center",
+  borderRadius: "0.5rem",
+  boxShadow: "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)",
+};
+
+const bodyContainerCardImg = {
+  width: "10rem",
+  height: "10rem",
+};
+const bodyContainerCardH3 = {
+  width: "100%",
+  textAlign: "center",
+  backgroundColor: "#01BAEF",
+  color: "#FBFBFF",
+  marginBottom: "0.5rem",
+  textTransform: "capitalize",
+  letterSpacing: "0.2rem",
+  fontSize: "1.2rem",
 };
 
 class ListOfPokemons extends React.Component {
@@ -59,6 +100,23 @@ class ListOfPokemons extends React.Component {
         </div>
 
         {/* Header - Ends Here -*/}
+
+        <div style={bodyContainer}>
+          <div style={bodyContainer}>
+            {this.props.pokemon.map((data) => (
+              <div style={bodyContainerCard}>
+                <div style={bodyContainerCardH3}>
+                  <h3>{data.name}</h3>
+                </div>
+                <img
+                  style={bodyContainerCardImg}
+                  src={data.img}
+                  alt={data.name}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
